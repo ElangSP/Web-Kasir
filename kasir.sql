49fb7379e335f18302615719a108974aad4a1dbc
ElangSP <elangsaputra151208@gmail.com>
2025-10-21 10:17:19 +0700

    Add files via upload

---
+++ kasir.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2025 at 05:10 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table kasir.products: #1932 - Table &#039;kasir.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table kasir.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kasir`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+-- Error reading structure for table kasir.sales: #1932 - Table &#039;kasir.sales&#039; doesn&#039;t exist in engine
+-- Error reading data for table kasir.sales: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kasir`.`sales`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_items`
+--
+-- Error reading structure for table kasir.sale_items: #1932 - Table &#039;kasir.sale_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table kasir.sale_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kasir`.`sale_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table kasir.users: #1932 - Table &#039;kasir.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table kasir.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kasir`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
